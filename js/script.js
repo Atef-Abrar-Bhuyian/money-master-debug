23703abfbda5d02729361d00b4b01e004b81b4a3
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-10-05 19:29:12 +0600

    save calculation bug fixed

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ const calculateSavings = () => {
   if (savePercentage < 0) {
     alert("Provide positive saving value");
   }
-  const savingAmount = (savePercentage / 100) + income;
+  const savingAmount = (savePercentage / 100) * income.value;
 
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
